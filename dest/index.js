16b2686ad25f4aba7740662cf7422f93a675e608
John Anthony Rivera <jrivera@chefsteps.com>
2017-10-31 13:11:15 -0700

    Shim window if not present

--- dest/index.js
+++ dest/index.js
@@ -1,3 +1,12 @@
+if (typeof window === 'undefined') {
+  var window = {
+    location: {
+      host: 'react-native',
+    },
+  };
+  window.window = window;
+}
+
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.bunyan = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (Buffer,process){
 /**
@@ -3685,7 +3694,7 @@ module.exports={
   "_args": [
     [
       "bunyan@2.0.2",
-      "/Users/jrivera/chefsteps/forks/browser-bunyan"
+      "/Users/jrivera/chefsteps/forks/react-native-bunyan"
     ]
   ],
   "_from": "bunyan@2.0.2",
@@ -3719,7 +3728,7 @@ module.exports={
   "_shasum": "8dbb0feb1a320b9255bc42ba2d4460ad6c045028",
   "_shrinkwrap": null,
   "_spec": "bunyan@2.0.2",
-  "_where": "/Users/jrivera/chefsteps/forks/browser-bunyan",
+  "_where": "/Users/jrivera/chefsteps/forks/react-native-bunyan",
   "author": {
     "email": "trentm@gmail.com",
     "name": "Trent Mick",
--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,42 @@ var del = require('del');
 var util = require('util');
 var rename = require('gulp-rename');
 var insert = require('gulp-insert');
+var concat = require('gulp-concat');
 
-var sourceFile = './node_modules/bunyan/lib/bunyan.js'
+var sourceFile = './node_modules/bunyan/lib/bunyan.js';
 
-gulp.task('clean', function () {
-    return del('./dist/**/*.*');
+gulp.task('clean', function() {
+  return del(['./dist/**/*.*', './build/**/*.*']);
+});
+
+gulp.task('browserify', ['clean'], function() {
+  return browserify({
+    entries: sourceFile,
+    standalone: 'bunyan',
+  })
+    .bundle()
+    .pipe(vinylStream('bunyan.js'))
+    .pipe(gulp.dest('./build'));
 });
 
 //package with browserify
-gulp.task('package', [ 'clean' ], function() {
-    return browserify({
-        entries: sourceFile,
-        standalone: 'bunyan'
-    })
-        .bundle()
-        .pipe(vinylStream('index.js'))
-        .pipe(gulp.dest('.'));
+gulp.task('package', ['clean', 'browserify'], function() {
+  return gulp.src([ './lib/windowShim.js', './build/bunyan.js'])
+    .pipe(concat('index.js'))
+    .pipe(gulp.dest('./dest'));
 });
 
-gulp.task('minify', [ 'package' ], function() {
-
-    var packageMessageTempalte =
-        '/**\n' +
-        ' * %s - %s\n' +
-        ' * %s\n' +
-        ' * %s\n' +
-        ' */\n';
-
-    var meta = require('./package.json');
-    var packageMessage = util.format(packageMessageTempalte, meta.name, meta.version, meta.description, meta.licenses[0].type);
-
-    return gulp.src('./index.js')
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(uglify())
-        .pipe(insert.prepend(packageMessage))
-        .pipe(gulp.dest('.'));
+gulp.task('minify', ['package'], function() {
+  return gulp
+    .src('./index.js')
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['minify']);
